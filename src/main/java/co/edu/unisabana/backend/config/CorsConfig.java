c341d537187c0c6b680375d68d32c4edf8fe8aa1
Ana3Lu <anaquiva@unisabana.edu.co>
2025-02-16 16:04:53 -0500

    Cambia la URL del frontend

--- src/main/java/co/edu/unisabana/backend/config/CorsConfig.java
+++ src/main/java/co/edu/unisabana/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://152.42.154.220", "https://kubernetes-83j6uq10p-juan-cetinas-projects.vercel.app")
+                        .allowedOrigins("http://152.42.154.220", "https://k8s-patrones.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
