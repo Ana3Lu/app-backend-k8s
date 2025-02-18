6d0e3d7e937883014d83c5d7f6fb2cec89c84121
Ana3Lu <anaquiva@unisabana.edu.co>
2025-02-18 07:46:23 -0500

    Agrega nuevo dominio a CORS

--- src/main/java/co/edu/unisabana/backend/config/CorsConfig.java
+++ src/main/java/co/edu/unisabana/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://152.42.154.220", "https://k8s-patrones.vercel.app")
+                        .allowedOrigins("http://152.42.154.220", "https://k8s-patrones.vercel.app", "http://apik8s.duckdns.org")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
